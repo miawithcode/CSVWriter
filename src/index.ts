8e0610787beafeafa850784934323f4971f5f24e
miawithcode <cuttontail@gmail.com>
2024-03-13 01:07:35 +0800

    feat: format row data

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,27 @@ type PaymentColumns = ('id' | 'amount' | 'to' | 'notes')[];
 
 class CSVWriter {
   constructor(private columns: PaymentColumns) {
-    this.csv = this.columns.join(',');
+    this.csv = this.columns.join(',') + '\n';
   }
 
   private csv: string;
+
+  addRows(values: Payment[]): void {
+    let rows = values.map((v) => this.formatRow(v));
+
+    this.csv += rows.join('\n');
+
+    console.log(this.csv);
+  }
+
+  private formatRow(p: Payment): string {
+    return this.columns.map((col) => p[col]).join(',');
+  }
 }
 
 const writer = new CSVWriter(['id', 'amount', 'to', 'notes']);
+
+writer.addRows([
+  { id: 1, amount: 50, to: 'yoshi', notes: 'for design work' },
+  { id: 2, amount: 10, to: 'mario', notes: 'for dev work' },
+]);
